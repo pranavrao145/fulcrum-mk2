14e5059f16a5e706506a4ad9846a620599f3f436
Pranav Rao <pranavrao145@gmail.com>
2021-05-30 11:18:36 -0400

    added database

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,49 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 // import all necessary modules
-import Discord from 'discord.js' 
-//import fs from 'fs'
-//import schedule from 'node-schedule'
+import Discord from 'discord.js';
+import { Client } from 'pg';
 
-const client = new Discord.Client();
+// declare all necessary constants
+const client: any = new Discord.Client(); // declare client object
+const prefix = "f!"; // declare prefix
 
-client.login(process.env.BOT_TOKEN).catch((err) => {
+// DATABASE CONNECTION
+
+// declare database connection
+const con = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    }
+})
+
+// establish database connection
+con.connect((err: any) => {
+    if (err) throw err;
+    console.log("Connected to database!");
+})
+
+// BOT EVENTS
+
+// log in as bot 
+client.login(process.env.BOT_TOKEN).catch((err: any) => {
     throw err;
 })
+
+// on ready, set status and log presence data
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user!.tag}!`);
+    console.log(client.guilds.cache.size)
+    client.user!.setPresence({
+        status: "online",
+        activity: {
+            name: "f!help",
+            type: "WATCHING",
+        },
+    })
+    .then((presence: any) => console.log(presence)).catch();
+});
+
+
 
