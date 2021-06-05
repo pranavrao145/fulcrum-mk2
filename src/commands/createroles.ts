f59cff79e2d37ed1d2194ed4ed4865df7984f3f1
Pranav Rao <pranavrao145@gmail.com>
2021-06-05 12:31:48 -0400

    finished createroles

--- src/commands/createroles.ts
+++ src/commands/createroles.ts
@@ -1,6 +1,7 @@
 import { ICommand } from '../utils/types';
 import { Message, MessageEmbed } from 'discord.js';
 import { Client } from 'pg';
+import {getChannelFromMention, getRoleFromMention, getUserFromMention} from '../utils/helpers';
 
 const command: ICommand = {
     name: 'createroles',
@@ -9,7 +10,6 @@ const command: ICommand = {
     syntax: 'f!createroles [role names (10 max)]', 
     async execute(message: Message, con: Client, args?: string[]) {
         console.log(`Command createroles started by user ${message.member!.user.tag} in guild ${message.guild!.name}.`);
-
 
         let outputEmbed = new MessageEmbed() // create an embed to display the results of the command
         .setColor('#FFFCF4')
@@ -38,5 +38,41 @@ const command: ICommand = {
                 console.log(e);
             }
         }
+
+        for (let roleName of args!) { // iterate through each of the role names given
+            roleName = roleName!.replace(/_/g, " "); // replce all underscores with spaces
+            if (getRoleFromMention(message, roleName!) || message.guild!.roles.cache.find(r => r.name === roleName) || getChannelFromMention(message, roleName!) || getUserFromMention(message, roleName!)) {
+                console.log(`Role with name ${roleName} already exists in guild. Skipping over it.`);
+                outputEmbedText += `\n**${roleName}:** Invalid role or role already exists on server.`;
+                continue;
+            }
+            try {
+                await message.guild!.roles.create({ // create the role with the needed data
+                    data: {
+                        name: roleName,
+                    }
+                });
+                outputEmbedText += `\n**${roleName}:** Role created successfully.`;
+                console.log(`Role ${roleName} created successfully in ${message.guild!.name}.`)
+            } catch (e) {
+                console.log(`Failed to create role ${roleName} in server ${message.guild!.name}. The error message is below:`)
+                console.log(e);
+                outputEmbedText += `\n**${roleName}:** Couldn't create role.`;
+            }
+        }
+
+        try { // send output embed with information about the command's success
+            outputEmbed.addField('\u200B', outputEmbedText); // add whatever text was accumulated throughout the command to the embed
+            if (outputEmbedText !== '') { // check if there is actually any text to send the embed with
+                outputEmbed.setDescription(`**Command executed by:** ${message.member!.user.tag}`);
+                await message.channel.send(outputEmbed);
+            }
+            console.log(`Command createroles, started by ${message.member!.user.tag}, terminated successfully in ${message.guild}.`);
+        } catch (e) {
+            console.log(`There was an error sending an embed in the guild ${message.guild}! The error message is below:`);
+            console.log(e);
+        }
     }
 }
+
+export = command; // export the command to the main module
